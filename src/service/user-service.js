cd45fd2a9866f278961e06a0194020429731717d
wrx <wrx@163.com>
2017-09-21 23:59:51 +0800

    用户信息重置

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -75,6 +75,24 @@ var _user = {
             error   : reject
         });
     },
+     // 获取用户信息
+    getUserInfo :  function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/user/get_information.do'),
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    updateUserInfo :  function(userInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/user/update_information.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
 	// 登出
 	logout : function(resolve,reject){
 		_mm.request({
